fe275b7317d6b80ef371d17b80b0122e823c31ff
Rokas Bendikas <ignasrokas@gmail.com>
2022-09-13 08:26:50 +0100

    Minor changes

--- src/components/publications/Publications.jsx
+++ src/components/publications/Publications.jsx
@@ -4,16 +4,12 @@ import PublicationList from "../../assets/publications.json"
 import {AiFillFilePdf} from 'react-icons/ai'
 import {RiFilePaperLine} from "react-icons/ri"
 
-import pdf from "../../assets/ConstructingAHumanAtrialFibreAtlas.pdf"
-
-
 const Publications = (props) => {
 
     return(
         <div className="section-container">
             <div className="container-row">
                 <h1>Publications</h1>
-                <a href={pdf} ><AiFillFilePdf /></a>
             </div>
 
             <div id="publications-body" className="container-row">
@@ -29,8 +25,8 @@ const Publications = (props) => {
                                     <i>{publication.venue}</i>, {publication.year}
                                 </div>
                                 <div className="publication-icons">
-                                    <a href={pdf} ><AiFillFilePdf /></a>
-                                    <a href="" ><RiFilePaperLine /></a>
+                                    {console.log(publication.pdf)}
+                                    <a href={require('../../assets/ConstructingAHumanAtrialFibreAtlas.pdf')} target="_blank" rel="noreferrer"><AiFillFilePdf /></a>
                                 </div>
                             </div>
                         )
