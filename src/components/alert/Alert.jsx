011d57f9441f05b6814dc49b425895b0e2b59e99
Rokas Bendikas <41422663+rokas-bendikas@users.noreply.github.com>
2022-09-16 13:11:32 +0100

    Added News section, some minor fixes

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -3,14 +3,32 @@ import React from 'react'
 import "./Alert.css"
 
 function BottomAlert() {
+
+  let width = window.innerWidth;
+  let MyAlert;
+
+  console.log(width)
+
+  if (width > 650) {
+    MyAlert = (
+      <div id="bottom-alert">
+        <div className="alert alert-custom">
+            ©2022 Rokas Bendikas. The website is built using <a href="https://reactjs.org/" target="_black">React</a>, 
+            <a href="https://getbootstrap.com/" target="_black"> Bootstrap</a> and 
+            <a href="https://www.w3schools.com/css/" target="_black"> CSS3</a>.
+        </div>
+    </div>);
+  } else {
+    MyAlert = (
+      <div id="bottom-alert">
+        <div className="alert alert-custom">
+            ©2022 Rokas Bendikas.
+        </div>
+    </div>);
+  }
+
   return (
-    <div id="bottom-alert">
-            <div className="alert alert-custom">
-                ©2022 Rokas Bendikas. The website is built using <a href="https://reactjs.org/" target="_black">React</a>, 
-                <a href="https://getbootstrap.com/" target="_black"> Bootstrap</a> and 
-                <a href="https://www.w3schools.com/css/" target="_black"> CSS3</a>.
-            </div>
-    </div>
+    MyAlert
   )
 }
 
--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -3,28 +3,93 @@ import "./News.css"
 const News = (props) => {
     return(
         <div className="section-container">
-            <div className="container-row">
-                <div className="container-column">
-                        <h1>News</h1>
-                    </div>
-                </div>
-            <div className="container-row">
-                <div className="container-column">
-                    <div className="news-item">
-                        <div className="news-date">
-                            Jun 2,
-                            <br />
-                            2022
-                        </div>
-                    </div>
 
+            <div id="news-title" className="container-row">
                 <div className="container-column">
-                    <div className="news-content">
-                        Accepted an offer for a fully-funded PhD in Foundational AI, supervised by Prof. Danail Stoyanov and Dr. Francisco Vasconcelos at University College London.
-                    </div>
-                </div>
+                    <h1>News</h1>
                 </div>
             </div>
+
+            <div id="news-item" className="container-row">
+                <div className="container-column">
+                    <div id="news-date">
+                        Jun 20,
+                        <br />
+                        2022
+                    </div>
+                </div>
+
+                <div id="news-separator" className="container-column">
+
+                </div>
+
+                <div className="container-column">
+                    <div id="news-content">
+                        Started a Software Engineering internship <a href="https://uk.mathworks.com/" target="_blank" rel="noreferrer">@MathWorks</a> Deep Learning team.
+                    </div>
+                </div>
+            </div>
+
+            <div id="news-item" className="container-row">
+                <div className="container-column">
+                    <div id="news-date">
+                        Jun 2,
+                        <br />
+                        2021
+                    </div>
+                </div>
+
+                <div id="news-separator" className="container-column">
+
+                </div>
+
+                <div className="container-column">
+                    <div id="news-content">
+                        Accepted an offer from <a href="https://www.ucl.ac.uk/" target="_blank" rel="noreferrer">University College London</a> for a fully-funded PhD in Foundational AI <a href="https://www.ucl.ac.uk/surgical-robot-vision/surgical-robot-vision" target="_blank" rel="noreferrer">@Surgical Robot Vision group</a>, supervised by Prof. Danail Stoyanov.
+                    </div>
+                </div>
+            </div>
+
+            <div id="news-item" className="container-row">
+                <div className="container-column">
+                    <div id="news-date">
+                        Aug 1,
+                        <br />
+                        2020
+                    </div>
+                </div>
+
+                <div id="news-separator" className="container-column">
+
+                </div>
+
+                <div className="container-column">
+                    <div id="news-content">
+                        Awarded BEng in Biomedical Engineering from <a href="https://www.kcl.ac.uk/" target="_blank" rel="noreferrer">King’s College London </a> with distinction.
+                    </div>
+                </div>
+            </div>
+
+            <div id="news-item" className="container-row">
+                <div className="container-column">
+                    <div id="news-date">
+                        Jun 21,
+                        <br />
+                        2020
+                    </div>
+                </div>
+
+                <div id="news-separator" className="container-column">
+
+                </div>
+
+                <div className="container-column">
+                    <div id="news-content">
+                        Accepted an offer from <a href="https://www.imperial.ac.uk/" target="_blank" rel="noreferrer">Imperial College London</a> for MSc Computing (Artificial Intelligence and Machine Learning) course.
+                    </div>
+                </div>
+            </div>
+
         </div>
     );   
 }
